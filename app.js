7a6a0efc1ca698dc9d3af41b487127c048e62311
Academik1986 <94170986+Academik1986@users.noreply.github.com>
2022-10-03 18:05:22 +0300

    New shop

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+// Событие клика 
+
+// const nav = document.querySelectorAll(".nav")
+
+//  for (let navItem of nav) {
+//     navItem.addEventListener("click", function(){
+
+//         console.log("clicked");})
+ 
+ 
+//     }
+
+// const nav = document.querySelectorAll(".nav__link")
+
+//  for (let navItem of nav) {
+//     navItem.addEventListener("click", function(){
+
+//         console.log(navItem.text);})
+ 
+ 
+//     }
